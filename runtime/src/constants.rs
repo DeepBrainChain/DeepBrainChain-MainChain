fd9d752a2befaf22bf9851522dcf3d0eaa618c9b
bobo <dongkaige@gmail.com>
2021-01-20 19:21:45 +0800

    feat: 更改token单位精度

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -21,9 +21,10 @@
 pub mod currency {
     use node_primitives::Balance;
 
-    pub const MILLICENTS: Balance = 1_000_000_000;
-    pub const CENTS: Balance = 1_000 * MILLICENTS; // assume this is worth about a cent.
-    pub const DOLLARS: Balance = 100 * CENTS;
+    pub const DBCS: Balance = 1_000_000_000_000_000;
+    pub const DOLLARS: Balance = DBCS / 100; // 10_000_000_000_000
+    pub const CENTS: Balance = DOLLARS / 100; // 100_000_000_000
+    pub const MILLICENTS: Balance = CENTS / 1_000; // 100_000_000
 
     pub const fn deposit(items: u32, bytes: u32) -> Balance {
         items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
