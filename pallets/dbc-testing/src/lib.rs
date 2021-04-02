3955ad4f1fef41a899d66495aa275fe8a3dc972b
bobo <dongkaige@gmail.com>
2021-04-02 19:07:27 +0800

    feat: 增加ss58地址转换测试函数

--- pallets/dbc-testing/src/lib.rs
+++ pallets/dbc-testing/src/lib.rs
@@ -5,6 +5,7 @@ use frame_support::traits::Currency;
 use frame_support::{debug, decl_module, decl_storage, dispatch::DispatchResult};
 use frame_system::{self as system, ensure_root, ensure_signed};
 use phase_reward::PhaseReward;
+use sp_std::{convert::TryInto, str};
 
 type BalanceOf<T> =
     <<T as Config>::Currency as Currency<<T as system::Config>::AccountId>>::Balance;
@@ -64,5 +65,32 @@ decl_module! {
             T::PhaseReward::set_phase2_reward(reward_balance);
             Ok(())
         }
+    }
+}
+
+impl<T: Config> Module<T> {
+    pub fn ss58_decode(s: &str) -> Option<[u8; 32]> {
+        const CHECKSUM_LEN: usize = 2;
+        let mut data: [u8; 36] = [0; 36];
+        let decoded = bs58::decode(s).into(&mut data);
+        debug::info!("####### decoded2: {:?}, {:?}", decoded, data);
+
+        let (prefix_len, ident) = match data[0] {
+            0..=63 => (1, data[0] as u16),
+            64..=127 => {
+                // let lower = (data[0] << 2) | (data[1] >> 6);
+                // let upper = data[1] & 0b00111111;
+                // (2, (lower as u16) | ((upper as u16) << 8))
+
+                // TODO: should support in the future
+                (1, 1)
+            }
+            _ => (1, 1), // TODO: return error here
+        };
+
+        return Some(data[1..35].try_into().unwrap());
+
+        // return Some(data[1..34].try_into());
+        // let public_key = data[1..34].clone();
     }
 }
