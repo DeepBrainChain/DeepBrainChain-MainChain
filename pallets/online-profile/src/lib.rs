bd4fc8246962499a4520fe1f5bf8363087e2249a
bobo <dongkaige@gmail.com>
2021-07-19 10:00:27 +0800

    fix: update stash rentedGpuNum after rent

--- pallets/online-profile/src/lib.rs
+++ pallets/online-profile/src/lib.rs
@@ -1524,12 +1524,19 @@ impl<T: Config> RTOps for Pallet<T> {
 
                 machine_info.total_rented_times += 1;
 
-                if sys_info.total_rented_gpu.checked_add(machine_gpu_num).is_none() {
+                if let Some(new_gpu_num) = sys_info.total_rented_gpu.checked_add(machine_gpu_num) {
+                    sys_info.total_rented_gpu = new_gpu_num;
+                } else {
                     debug::error!("Add sysInfo total rented gpu failed");
-                };
-                if stash_machine.total_rented_gpu.checked_add(machine_gpu_num).is_none() {
+                }
+
+                if let Some(new_gpu_num) =
+                    stash_machine.total_rented_gpu.checked_add(machine_gpu_num)
+                {
+                    stash_machine.total_rented_gpu = new_gpu_num;
+                } else {
                     debug::error!("Add stashMachine total rented gpu failed");
-                };
+                }
 
                 Self::change_pos_gpu_by_rent(machine_id, true);
             }
@@ -1540,10 +1547,18 @@ impl<T: Config> RTOps for Pallet<T> {
                     Self::update_snap_by_rent_status(machine_id.to_vec(), false);
                     machine_info.total_rented_duration += rent_duration.unwrap();
 
-                    if sys_info.total_rented_gpu.checked_sub(machine_gpu_num).is_none() {
+                    if let Some(new_gpu_num) =
+                        sys_info.total_rented_gpu.checked_sub(machine_gpu_num)
+                    {
+                        sys_info.total_rented_gpu = new_gpu_num;
+                    } else {
                         debug::error!("Sub sysInfo total rented gpu failed");
                     };
-                    if stash_machine.total_rented_gpu.checked_sub(machine_gpu_num).is_none() {
+                    if let Some(new_gpu_num) =
+                        stash_machine.total_rented_gpu.checked_sub(machine_gpu_num)
+                    {
+                        stash_machine.total_rented_gpu = new_gpu_num;
+                    } else {
                         debug::error!("Sub stashMachine total rented gpu failed");
                     };
 
