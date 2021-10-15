197d75da1c439795ea0e246c7df322c8d6810178
bobo <dongkaige@gmail.com>
2021-10-15 18:45:37 +0800

    #24: add todo

--- pallets/online-profile/src/lib.rs
+++ pallets/online-profile/src/lib.rs
@@ -796,7 +796,7 @@ pub mod pallet {
             Ok(().into())
         }
 
-        // FIXME: should cancel slash and slash reward
+        // TODO: should cancel slash and slash reward
         #[pallet::weight(0)]
         pub fn cancel_slash(origin: OriginFor<T>, slash_id: u64) -> DispatchResultWithPostInfo {
             T::CancelSlashOrigin::ensure_origin(origin)?;
