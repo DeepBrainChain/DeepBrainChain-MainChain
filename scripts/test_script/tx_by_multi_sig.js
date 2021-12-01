6c3cb97edb0a04d8bc3c2b80a2bacf05da330535
bobo <dongkaige@gmail.com>
2021-12-01 17:18:52 +0800

    fix: multi-sig can work

--- scripts/test_script/tx_by_multi_sig.js
+++ scripts/test_script/tx_by_multi_sig.js
@@ -1,24 +1,35 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { Keyring } from "@polkadot/keyring";
-// import fs from "fs";
+import fs from "fs";
 import minimist from "minimist";
 import { blake2AsHex } from "@polkadot/util-crypto";
 
-const websocket = "ws://127.0.0.1:9944";
+// 不需要变
 const typeFile = "../../dbc_types.json";
 const callIndex = "0x0603";
 const maxWeight = 194407000;
 
+// 初始化一次即可
+const websocket = "ws://127.0.0.1:9944";
+const allAccount = [
+  "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
+  "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty",
+];
 const threshold = 2;
-const allAccount = [];
 
-var destAccount = "";
-var transAmount = "";
+// 每次转账之前设置
+var destAccount = "5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy";
+var transAmount = "10000000000000000"; // 10**15 * 10 = 10 DBC
 
-var signerKey = ""; // 签名账户
+// 第一次执行脚本之后设置
 var firstCallHeight = 12;
 var firstCallIndex = 1;
-var isFirstCall = fasle;
+var isFirstCall = false;
+
+var signerKey =
+  "0xe5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a"; // 签名账户
+
+// 最后一次执行时设置
 var isFinalSign = false;
 
 async function main() {
@@ -29,7 +40,7 @@ async function main() {
 
   // 构建连接
   const wsProvider = new WsProvider(websocket);
-  const type_json = JSON.parse(typeFile);
+  const type_json = JSON.parse(fs.readFileSync(typeFile));
 
   // Create the API and wait until ready
   const api = await ApiPromise.create({
@@ -59,24 +70,23 @@ async function main() {
   console.log("### encodedCall: ", encodedProposal2);
   console.log("### callHash: ", encodedHash);
 
-  if (isFirstCall) {
-    timepoint = null;
-  } else {
+  var timepoint = null;
+  if (!isFirstCall) {
     timepoint = { height: firstCallHeight, index: firstCallIndex };
   }
 
-  // TODO:
-  const otherAccount = allAccount;
+  // TODO: 当前改这个
+  const otherAccount = ["5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty"];
 
   if (isFinalSign) {
     // 最后一次多签签名
-    callFunc = api.tx.multisig.asMulti;
+    const callFunc = api.tx.multisig.asMulti;
     await do_sign_tx(
       callFunc,
-      accountFromKeyring2,
+      accountFromKeyring,
       nonce,
       threshold,
-      [account1],
+      otherAccount,
       { height: 32, index: 1 },
       encodedProposal2,
       false,
@@ -84,14 +94,14 @@ async function main() {
     ).catch((error) => console.log(error.message));
   } else {
     // 第一次多签签名
-    callFunc = api.tx.multisig.approveAsMulti;
+    const callFunc = api.tx.multisig.approveAsMulti;
     await do_sign_tx(
-      callFunc1,
-      accountFromKeyring1,
+      callFunc,
+      accountFromKeyring,
       nonce,
       threshold,
-      [account2],
-      timepoint,
+      otherAccount,
+      null,
       encodedHash,
       maxWeight
     ).catch((error) => console.log(error.message));
