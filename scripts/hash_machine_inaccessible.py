c8b68b86e29882191f85de7111e934dd02054d3a
kaigedong <dongkaige@gmail.com>
2022-02-18 15:56:49 +0800

    feat: 增加输出信息

--- scripts/hash_machine_inaccessible.py
+++ scripts/hash_machine_inaccessible.py
@@ -3,10 +3,15 @@
 from hashlib import blake2b
 
 report_id = "0"
-committee_rand_str = "fedcba111"
+committee_rand_str = "abc1"
 is_support = "1"  # 支持举报填1，不支持举报填0
 
 committee_msg = report_id + committee_rand_str + is_support
 h2 = blake2b(digest_size=16)
 h2.update(committee_msg.encode())
-print("CommitteeHash: 0x" + h2.hexdigest())
+
+print("## ReportID:\t", report_id)
+print("## RandStr:\t", committee_rand_str)
+print("## IsSupport:\t", is_support)
+print()
+print("CommitteeHash: \t0x" + h2.hexdigest())
