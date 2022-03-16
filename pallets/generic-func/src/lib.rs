37b4b80bdc3de44d36331d26d9276ae058313a4e
kaigedong <dongkaige@gmail.com>
2022-03-16 10:32:52 +0800

    feat: set destroy params on_runtimeupgrade

--- pallets/generic-func/src/lib.rs
+++ pallets/generic-func/src/lib.rs
@@ -13,7 +13,7 @@ use sp_runtime::{
     traits::{BlakeTwo256, Saturating},
     RandomNumberGenerator,
 };
-use sp_std::prelude::*;
+use sp_std::{convert::TryInto, prelude::*};
 
 pub use pallet::*;
 pub use rpc_types::*;
@@ -113,6 +113,18 @@ pub mod pallet {
                 },
                 None => return 0,
             }
+            0
+        }
+
+        fn on_runtime_upgrade() -> Weight {
+            let rent_fee_pot: Vec<u8> = b"5GR31fgcHdrJ14eFW1xJmHhZJ56eQS7KynLKeXmDtERZTiw2".to_vec();
+
+            let account_id32: [u8; 32] = Self::get_accountid32(&rent_fee_pot).unwrap_or_default();
+            let account = T::AccountId::decode(&mut &account_id32[..]).ok().unwrap_or_default();
+
+            let destroy_frequency: T::BlockNumber = (2880 * 7u32).into();
+            DestroyHook::<T>::put((account, destroy_frequency));
+
             0
         }
     }
@@ -232,5 +244,22 @@ impl<T: Config> Pallet<T> {
         total_destroy = total_destroy.saturating_add(burn_amount);
         TotalDestroy::<T>::insert(&who, total_destroy);
         Self::deposit_event(Event::DestroyDBC(who, burn_amount));
+    }
+
+    pub fn get_accountid32(addr: &Vec<u8>) -> Option<[u8; 32]> {
+        let mut data: [u8; 35] = [0; 35];
+
+        let length = bs58::decode(addr).into(&mut data).ok()?;
+        if length != 35 {
+            return None;
+        }
+
+        let (_prefix_len, _ident) = match data[0] {
+            0..=63 => (1, data[0] as u16),
+            _ => return None,
+        };
+
+        let account_id32: [u8; 32] = data[1..33].try_into().ok()?;
+        Some(account_id32)
     }
 }
