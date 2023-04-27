1c62d44c15cc0a386a489350ea3ae8f31c85df57
kaigedong <dongkaige@gmail.com>
2023-04-27 18:33:48 +0800

    fix: 添加部分模块升级

--- pallets/online-profile2/src/lib.rs
+++ pallets/online-profile2/src/lib.rs
@@ -1,17 +1,32 @@
 #![recursion_limit = "256"]
 #![cfg_attr(not(feature = "std"), no_std)]
 
+mod types;
+
+use dbc_support::{
+    live_machine::LiveMachine,
+    machine_info::MachineInfo,
+    machine_type::{Latitude, Longitude, MachineStatus, StakerCustomizeInfo},
+    traits::{DbcPrice, GNOps, ManageCommittee},
+    verify_online::StashMachine,
+    verify_slash::{OPPendingSlashInfo, OPPendingSlashReviewInfo, OPSlashReason},
+    EraIndex, ItemList, MachineId, SlashId, ONE_DAY,
+};
 use frame_support::{
     pallet_prelude::*,
-    traits::{Currency, OnUnbalanced, Randomness, ReservableCurrency},
+    traits::{Currency, EnsureOrigin, OnUnbalanced, Randomness, ReservableCurrency},
     weights::Weight,
 };
+use frame_system::pallet_prelude::*;
 
 type BalanceOf<T> =
     <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 type NegativeImbalanceOf<T> = <<T as Config>::Currency as Currency<
     <T as frame_system::Config>::AccountId,
 >>::NegativeImbalance;
+
+pub use pallet::*;
+pub use types::*;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -30,13 +45,19 @@ pub mod pallet {
             Balance = BalanceOf<Self>,
         >;
         type Slash: OnUnbalanced<NegativeImbalanceOf<Self>>;
-        type CancelSlashOrigin: EnsureOrigin<Self::Origin>;
+        type CancelSlashOrigin: EnsureOrigin<Self::RuntimeOrigin>;
         type SlashAndReward: GNOps<AccountId = Self::AccountId, Balance = BalanceOf<Self>>;
     }
 
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
+    #[pallet::without_storage_info]
     pub struct Pallet<T>(_);
+
+    #[pallet::storage]
+    #[pallet::getter(fn phase_reward_info)]
+    pub(super) type PhaseRewardInfo<T: Config> =
+        StorageValue<_, PhaseRewardInfoDetail<BalanceOf<T>>>;
 
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
