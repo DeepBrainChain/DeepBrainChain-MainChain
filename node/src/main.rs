0c59714d1d0456df65f6c0aa60542348a4f0b588
frank0528 <963688200@qq.com>
2024-06-30 12:21:01 +0800

    add sandbox host functions

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,6 +1,6 @@
 // This file is part of Substrate.
 
-// Copyright (C) 2019-2022 Parity Technologies (UK) Ltd.
+// Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
 // This program is free software: you can redistribute it and/or modify
@@ -16,11 +16,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! A set of testing utilities for Substrate Node.
+//! Substrate Node CLI
 
 #![warn(missing_docs)]
 
-pub mod bench;
-pub mod client;
-pub mod genesis;
-pub mod keyring;
+fn main() -> sc_cli::Result<()> {
+    dbc_cli::run()
+}
