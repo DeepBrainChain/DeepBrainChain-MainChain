d5bc77c4c8b45879b633188a63374dcfff19b5fd
Frank <963688200@qq.com>
2024-07-04 15:42:35 +0800

    Change ExistentialDeposit to 0
    Metamask can use the full fee balance

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -457,7 +457,7 @@ impl pallet_indices::Config for Runtime {
 }
 
 parameter_types! {
-    pub const ExistentialDeposit: Balance = 1 * DOLLARS;
+    pub const ExistentialDeposit: Balance = 0;
     // For weight estimation, we assume that the most locks on an individual account will be 50.
     // This number may need to be adjusted in the future if this assumption no longer holds true.
     pub const MaxLocks: u32 = 50;
