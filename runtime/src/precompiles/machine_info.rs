3f49ac0d3f1070feede237418975db9bce748521
robin <174920143@qq.com>
2025-01-08 18:31:02 +0800

    Precompile machie info (#187)
    * fix
    
    * fix
    
    * fmt

--- runtime/src/precompiles/machine_info.rs
+++ runtime/src/precompiles/machine_info.rs
@@ -275,7 +275,7 @@ where
                     &[
                         ethabi::ParamType::String,    // machine_id
                         ethabi::ParamType::Uint(256), // rent_block_numbers
-                        ethabi::ParamType::Uint(8),   // rent_gpu_count
+                        ethabi::ParamType::Uint(256), // rent_gpu_count
                     ],
                     &input.get(4..).unwrap_or_default(),
                 )
@@ -338,7 +338,7 @@ where
                     &[
                         ethabi::ParamType::String,    // machine_id
                         ethabi::ParamType::Uint(256), // rent_block_numbers
-                        ethabi::ParamType::Uint(8),   // rent_gpu_count
+                        ethabi::ParamType::Uint(256), // rent_gpu_count
                     ],
                     &input.get(4..).unwrap_or_default(),
                 )
@@ -401,7 +401,7 @@ where
                     &[
                         ethabi::ParamType::String,    // machine_id
                         ethabi::ParamType::Uint(256), // rent_block_numbers
-                        ethabi::ParamType::Uint(8),   // rent_gpu_count
+                        ethabi::ParamType::Uint(256), // rent_gpu_count
                     ],
                     &input.get(4..).unwrap_or_default(),
                 )
@@ -463,8 +463,8 @@ where
                     &[
                         ethabi::ParamType::Uint(256), // calc_point
                         ethabi::ParamType::Uint(256), // rent_block_numbers
-                        ethabi::ParamType::Uint(8),   // rent_gpu_count
-                        ethabi::ParamType::Uint(8),   // total_gpu_count
+                        ethabi::ParamType::Uint(256), // rent_gpu_count
+                        ethabi::ParamType::Uint(256), // total_gpu_count
                     ],
                     &input.get(4..).unwrap_or_default(),
                 )
@@ -474,7 +474,7 @@ where
                 })?;
 
                 let calc_point_uint =
-                    param[1].clone().into_uint().ok_or_else(|| PrecompileFailure::Revert {
+                    param[0].clone().into_uint().ok_or_else(|| PrecompileFailure::Revert {
                         exit_status: ExitRevert::Reverted,
                         output: "decode param[0] failed".into(),
                     })?;
